b96b0671ee133cdaa0bf75b59604c5944e93c38d
Sereda <onegradus@mail.ru>
2019-03-28 16:38:00 +0300

    Work1

---
+++ JS/Work1.js
@@ -0,0 +1,25 @@
+let
+    a = 10;
+b =15;
+c = 12;
+console.log(2 * (a * b + b * c + a * c ));
+res = (a >= b) ? [(a === b) ? console.log("Высота равна ширине") : console.log("Высота больше ширины")] : console.log("Высота меньше ширины");
+
+let fazenda = 1000;
+let shirina = 15;
+let dlina = 25;
+let octatok;
+octatok = fazenda % (shirina * dlina);
+console.log(octatok);
+
+
+let oval = 15 * 100;
+let oval2 = 600;
+let koleco;
+koleco = oval - oval2;
+console.log("Площадь овала", oval, "СМ2");
+console.log("площадь овала" , oval2, "СМ2")
+console.log("Площадь полученного кольца:", koleco, "см2");
+
+
+
